4add08d96715f77a1deab8bd0dfaf3fa0e4b491e
Saurabh Kumar <55407524+Saurabh-1909@users.noreply.github.com>
2020-01-29 04:46:07 -0600

    gg

--- CucumberCaseStudy/src/test/java/Cucumber/RunnerClass.java
+++ CucumberCaseStudy/src/test/java/Cucumber/RunnerClass.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="Features", glue= {"Cucumber"}, tags="@login", plugin = {"pretty","json:target/cucumber.json" })
+@CucumberOptions(features="Features", glue= {"Cucumber"}, tags="@login")
 
 public class RunnerClass {
 
